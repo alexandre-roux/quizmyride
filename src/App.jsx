c05212522e6b8bb804e610ef7e8611953994dcde
Alexandre Roux <alexandre.roux@ensc.fr>
2025-07-23 15:28:06 +0200

    First version of the Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import {useState} from 'react'
-import './App.css'
+import './App.scss'
 import Home from "./components/Home.jsx";
 
 function App() {
-    const [displayHome, setDisplayHome] = useState(true)
 
     return (
         <div className="main-container">
-            {displayHome && (
                 <Home/>
-            )}
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
 const Home = () => {
+    const playGongSound = () => {
+        const audio = new Audio('/sounds/bvg-gong.mp3');
+        audio.play();
+    };
+
     return (
-        <h1>"Hello from Home"</h1>
+        <div className="home-container">
+            <h1>Welcome to Quiz My Ride</h1>
+            <img src="/images/logo.png" className="logo" alt="Quiz My Ride Logo"/>
+            <p className="intro-text">
+                Think you know the BVG bus fleet like the back of your Fahrkarte?<br/>
+                Can you tell a Citaro from a Citea faster than a bus at a green light?
+                <br/><br/>
+                Put your bus-spotting skills to the ultimate test in this quiz where only true bus nerds will
+                survive.<br/>
+                Guess the model, earn eternal respect, and maybe you’ll become the unofficial BVG Bus Master.
+            </p>
+            <button onClick={playGongSound}>
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+                Start the quiz
+            </button>
+        </div>
     );
 };
 
