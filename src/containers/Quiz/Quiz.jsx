b19aea3bf8d1370a4798779f207305ebedc292ae
Alexandre Roux <alexandre.roux@ensc.fr>
2025-09-08 19:04:47 +0200

    Move contents from components folder to containers folder

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -1,11 +1,15 @@
 import React, {useEffect} from 'react';
 import './Quiz.scss';
+import buses from '../../data/buses';
 
 const Quiz = () => {
+    const [selectedBuses, setSelectedBuses] = React.useState([]);
 
     useEffect(() => {
         // Create the quiz when the component mounts
-
+        // Create an array with 3 random elements from buses.js
+        const shuffled = [...buses].sort(() => Math.random() - 0.5);
+        setSelectedBuses(shuffled.slice(0, 3));
     }, [])
 
     return (<div className="quiz-container">
