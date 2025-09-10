587104aa5ddf5a1ec01b38ef24f8e457339ef18a
Alexandre Roux <alexandre.roux@ensc.fr>
2025-09-10 17:22:15 +0200

    Add the four answers as buttons

--- src/component/QuizCard.jsx
+++ src/component/QuizCard.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import './QuizCard.scss';
 
 const QuizCard = (props) => {
+    const answers = props.selectedBus && Array.isArray(props.selectedBus.answers)
+        ? props.selectedBus.answers
+        : [];
 
-    return (<div className="quiz-card">
-        <h2>What model is it?</h2>
-        <img src={props.selectedBus.image} alt={props.selectedBus.model}/>
-        <button>{props.selectedBus.model}</button>
-    </div>);
+    return (
+        <div className="quiz-card">
+            <h2>What model is it?</h2>
+            <img src={props.selectedBus.image} alt={props.selectedBus.model}/>
+            <div className="answers">
+                {answers.slice(0, 4).map((answer, idx) => (
+                    <button key={idx} type="button">
+                        {answer}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default QuizCard;
